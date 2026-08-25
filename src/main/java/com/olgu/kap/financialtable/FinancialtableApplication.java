eba18091eb4c207bdbe0e9046528ab3570cf95f4
Olgu Ay <olguay@192.168.1.105>
2026-08-25 23:47:43 +0300

    Initial commit

---
+++ src/main/java/com/olgu/kap/financialtable/FinancialtableApplication.java
@@ -0,0 +1,13 @@
+package com.olgu.kap.financialtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialtableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olgu/kap/financialtable/FinancialtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olgu.kap.financialtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialtableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
